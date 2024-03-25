6b836507c48e971678a5ae1ae7e3a4313090f799
unknown <apipska10@gmail.com>
2024-03-25 20:24:16 +0700

    Add OAuth SignIn

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,5 +1,6 @@
 'use client'
 import LoginControllers from "@/controllers/loginControllers"
+import { signIn } from "next-auth/react"
 export default function () {
     const {
         email, setEmail, password, setPassword, error, HandleSubmit
@@ -15,6 +16,11 @@ export default function () {
                 <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <button type="submit" className="btn btn-primary">LOGIN</button>
                 <span>Tidak mempunyai akun? <a href="/auth/signup">Daftar</a></span>
+                <div className="divider">
+                    OR
+                </div>
+                <button type="button" className="btn-google" onClick={() => signIn('google')}>Sign In with Google</button>
+                <button type="button" className="btn-github" onClick={() => signIn('github')}>Sign In with Github</button>
             </form>
         </>
     )
